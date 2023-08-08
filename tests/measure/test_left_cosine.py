359630f4c6e1e5bd4823c89d3637cdb7270c4e70
Ruben <ruben.m.menke@gmail.com>
2023-08-08 17:25:44 +0000

    not all working

--- tests/measure/test_left_cosine.py
+++ tests/measure/test_left_cosine.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
 
 from unittest import TestCase
-from simstring.measure.cosine import CosineMeasure
+from simstring.measure.left_cosine import LeftCosineMeasure
 
 
-class TestCosine(TestCase):
-    measure = CosineMeasure()
+class TestLeftCosineMeasure(TestCase):
+    measure = LeftCosineMeasure()
 
     def test_min_feature_size(self):
         self.assertEqual(self.measure.min_feature_size(5, 1.0), 5)
@@ -24,12 +24,12 @@ class TestCosine(TestCase):
         x = ["a", "ab", "bc", "c"]
         y = ["a", "ab", "bc", "cd", "e"]
         self.assertEqual(round(self.measure.similarity(x, x), 2), 1.0)
-        self.assertEqual(round(self.measure.similarity(x, y), 2), 0.67)
+        self.assertEqual(round(self.measure.similarity(x, y), 2), 0.75)
 
         z = ["a", "ab", "ba", "ab", "a"]
         self.assertEqual(round(self.measure.similarity(z, z), 2), 1.0)
-        self.assertEqual(round(self.measure.similarity(x, z), 2), 0.58)
-        self.assertEqual(round(self.measure.similarity(x, y), 2), 0.67)
+        self.assertEqual(round(self.measure.similarity(x, z), 2), 0.5)
+        self.assertEqual(round(self.measure.similarity(x, y), 2), 0.75)
 
         # Test as per paper trigrams with quotes of methyl sulphone and methyl sulfone
         a = [
@@ -70,11 +70,11 @@ class TestCosine(TestCase):
             'e" ',
         ]
         self.assertEqual(
-            round(self.measure.similarity(a, b), 3), 0.788
+            round(self.measure.similarity(a, b), 3), 0.812
         )
 
         name = ["Vl", "la", "ad", "di", "im", "mi", "ir", "r ", " P", "Pu", "ut", "ti", "in"]
         sentence = ["Do", "on", "na", "at", "ti", "io", "on", "n ", " t", "to", "o ", " t", "he", "e ", " g", "gr", "re", "ea", "at", "t ", " V", "Vl", "la", "ad", "di", "im", "mi", "ir", "r ", " P", "Pu", "ut", "ti", "in", "n ", " m", "my", "y ", " f", "fa", "av", "vo", "or", "ri", "it", "te", "e ", " p", "pr", "re", "es", "si", "id", "de", "en", "nt"]
         self.assertEqual(
-            round(self.measure.similarity(name, sentence), 2), 0.52
+            round(self.measure.similarity(name, sentence), 2), 1.0
         )
